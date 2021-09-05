23914970799e381e8371302aa1b44d3d961caae0
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2021-09-04 22:40:15 -0500

    Create Variety of operations.cpp

---
+++ Variety of operations.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int operations(){
+
+  int a = 0; int b = 0;
+
+  int c, d;
+	cin >> c >> d;
+
+   if (c==0 and d==0){
+      return 0;
+  }
+  if (c == d){
+      return 1;
+  }
+  // If the sum is odd the problem have no solution
+  if((c+d)&1){
+      return -1;
+  }
+  return 2;
+}
+
+int main(){
+
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--) {
+		cout<<operations()<<endl;
+	}
+
+   
+ return 0;
+}
