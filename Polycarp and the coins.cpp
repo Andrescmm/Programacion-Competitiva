d222046673d9290a612bff6b05bebc31e676e8b0
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2021-09-05 16:39:42 -0500

    good

---
+++ Polycarp and the coins.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+void polyCoins(){
+  int n;
+  cin>>n;
+
+  int c1 = n/3;
+  int c2 = n/3;
+  
+  if(n%3 == 1){
+      c1++;
+  }
+
+  if(n%3 == 2){
+      c2++;
+  }
+
+   cout<<c1<<" , "<<c2<<endl;
+   //cout<<"1 burle = "<<c1<<" , "<<"2 burles = "<<c2<<endl;
+}
+
+
+
+int main(){
+
+	int tc = 1;
+	cin >> tc;
+	while (tc--) {
+		polyCoins();
+	}
+
+}
