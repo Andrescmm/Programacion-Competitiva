56a4ea1816b9e4b6392270737efe5aeb9e53f815
Andres Cusirramos Marquez Mares <49249500+Andrescmm@users.noreply.github.com>
2021-09-13 03:18:44 -0500

    complete

--- Strange Function.cpp
+++ Strange Function.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
-#include<math.h>
-
 using namespace std;
+
+#define int long long
+
+const int modulo = 1e9 + 7;
+
+int gcd(int x, int y) {
+	while(y){int r = x%y; x = y; y = r;}
+  return x;
+}
+
+int lcm(int x, int y) {
+	return (x / gcd(x, y)) * y;
+}
+ 
 
 // f(i)
 int f(int i){  
@@ -26,14 +38,42 @@ int strangeFunction(){
     for(int i = 1; i<=n; i++){
           res += f(i);
     }
-
-    return res;
+    
+    cout<<"ans = ";
+    return res % modulo;
 }
 
-int main(){
+
+// Strange Function 2.0
+int sFunction(){
+        int n;
+        cin >> n;
+        
+        int lcm = 1;
+        int ans = 0;
+ 
+        for (int i = 2; ; i++) {
+            int bef = n / lcm;
+            lcm = lcm * i / gcd(lcm, i);
+            int aft = n / lcm;
+ 
+            ans += (bef - aft) * i;
+            ans %= modulo;
+ 
+            if (lcm > n) break;
+        }
+
+     cout<<"ans = ";
+     return ans;
+}
+  
+signed main(){
 	int tc = 1;
 	cin >> tc;
 	while (tc--) {
+         
         cout<<strangeFunction()<<endl;
+        //cout<<sFunction()<<endl;
 	}
+
 }
